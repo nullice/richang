bf5f586cff88444dde3e4b16b2ee6f2b457c3533
nullice <ui@nullice.com>
2017-09-05 22:23:55 +0800

    + README

---
+++ index.js
@@ -0,0 +1,10 @@
+import objectOBJ from "./src/objectOBJ"
+import stringSTR from "./src/stringSTR"
+import fileFIL from "./src/fileFIL"
+import typeTYP from "./src/typeTYP"
+import arrayARR from "./src/arrayARR"
+
+
+var Richang  = {stringSTR,fileFIL,typeTYP,arrayARR};
+
+export  default  Richang
