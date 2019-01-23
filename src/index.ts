581c81abd74c66feb7a6c89cf8501a4ac414a7b0
nullice <ui@Nullice.com>
2019-01-23 23:47:04 +0800

    去掉 @ 别名

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import shuffle from "lodash/shuffle"
-import * as event from "@/event/event"
-import * as geometry from "@/geometry/geometry"
+import * as event from "./event/event"
+import * as geometry from "./geometry/geometry"
 
 let Richang = {
     shuffle,
