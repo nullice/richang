ba5175c4e9cdf86d0e19b1cedce0ef85497b33ac
nullice <ui@Nullice.com>
2019-03-22 21:31:21 +0800

    添加 time.genTimestamp, timeDiff, timeDiffDay ...

--- src/time/time.ts
+++ src/time/time.ts
@@ -1,3 +1,5 @@
+import dayjs from "dayjs"
+
 /**
  * 创建一个秒表函数，秒表函数每次执行会返回执行时到创建时的时差
  *
@@ -16,4 +18,77 @@ export function stopwatch() {
         let now = new Date().getTime()
         return now - start
     }
+}
+
+/**
+ * 生成时间戳
+ *
+ * @example
+ * genTimestamp()    //=> 1521602474428
+ * @return {number}
+ */
+export function genTimestamp() {
+    return new Date().getTime()
+}
+
+/**
+ * 生成时间戳文本
+ * @example
+ * genTimestampText()  //=>  "JTJWZRH3"
+ * @return {string}
+ */
+export function genTimestampText() {
+    return genTimestamp()
+        .toString(36)
+        .toUpperCase()
+}
+
+/**
+ * 从时间戳/时间戳文本解析出 Date 对象
+ * @param timestamp
+ * @return {Date}
+ */
+export function parseTimestamp(timestamp: string | number): Date {
+    if (Number.isInteger(+timestamp)) {
+        return new Date(+timestamp)
+    } else {
+        return new Date(parseInt(<string>timestamp, 36))
+    }
+}
+// 'millisecond' | 'second' | 'minute' | 'hour' | 'day' | 'month' | 'year' | 'date'
+
+type TimeUnitType = "millisecond" | "second" | "minute" | "hour" | "day" | "month" | "year" | "date"
+
+/**
+ * 计算 2 个 Date 的时差
+ * @param date
+ * @param oldDate
+ * @param unit 时差单位
+ * @return {number}
+ */
+export function timeDiff(date: Date, oldDate: Date = new Date(), unit: TimeUnitType = "millisecond") {
+    let time1 = dayjs(date)
+    let time2 = dayjs(oldDate)
+    return time1.diff(time2, unit)
+}
+
+/**
+ * 计算 2 个 Date 的时差（天）
+ * @param date
+ * @param oldDate
+ * @return {number}
+ */
+export function timeDiffDay(date: Date, oldDate: Date = new Date()) {
+    return timeDiff(date, oldDate, "day")
+}
+
+
+/**
+ * 计算 2 个 Date 的时差（分钟）
+ * @param date
+ * @param oldDate
+ * @return {number}
+ */
+export function timeDiffMinute(date: Date, oldDate: Date = new Date()) {
+    return timeDiff(date, oldDate, "minute")
 }
---
+++ test/time/time.test.ts
@@ -0,0 +1,25 @@
+import { Richang as rc } from "../../src/index"
+
+describe("timeDiff", () => {
+    test("timeDiff millisecond", () => {
+        let t1 = new Date("2019-03-22T12:08:52.595Z")
+        let t2 = new Date("2019-03-22T12:08:51.595Z")
+        let d = rc.time.timeDiff(t1, t2)
+        expect(d).toBe(1000)
+    })
+
+    test("timeDiffDay 天", () => {
+        let t1 = new Date("2019-03-22T12:08:52.595Z")
+        let t2 = new Date("2019-03-21T12:08:51.595Z")
+        let d = rc.time.timeDiffDay(t1, t2)
+        expect(d).toBe(1)
+    })
+
+
+    test("timeDiffDay 分钟", () => {
+        let t1 = new Date("2019-03-22T12:10:51.595Z")
+        let t2 = new Date("2019-03-22T12:08:51.595Z")
+        let d = rc.time.timeDiffMinute(t1, t2)
+        expect(d).toBe(2)
+    })
+})
