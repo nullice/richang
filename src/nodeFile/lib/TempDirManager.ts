1a4c27322d02a0202a586dc0b8b027a252c9bc87
nullice <ui@nullice.com>
2019-05-10 21:04:35 +0800

    TempDirManager.ts

--- src/nodeFile/lib/TempDirManager.ts
+++ src/nodeFile/lib/TempDirManager.ts
@@ -5,6 +5,15 @@ const fsex = require("fs-extra")
 const fs = require("fs")
 const path = require("path")
 
+
+/**
+ * 临时目录管理器
+ *
+ * 能方便到到生产临时文件路径、写入临时文件
+ *
+ * 用 tempDirManager.clear([day:number]) 能按删除指定天数之前到临时文件
+ *
+ */
 export class TempDirManager {
     // 系统临时目录
     sysTempDir: string
@@ -15,8 +24,8 @@ export class TempDirManager {
     // 临时目录创建时间
     timestamp: string
 
-    constructor(name: string) {
-        this.sysTempDir = os.tmpdir()
+    constructor(name: string, sysTempDir?: string) {
+        this.sysTempDir = sysTempDir ? sysTempDir : os.tmpdir()
 
         // 创建主临时文件夹
         this.mianTempDir = path.join(this.sysTempDir, name)
@@ -37,14 +46,14 @@ export class TempDirManager {
 
     /**
      * 申请一个临时文件路径
-     *@example
+     * @example
      * newTempFilePath() // => "c:/temp/xd123fs43aew"
      * // 可用提供一个子文件夹名
      * genTempFilePath("static") // => "c:/temp/static/xd123fs43aew"
-     * @param subDir
+     * @param subDir 子文件夹名
      * @return {any}
      */
-    newTempFilePath(subDir: string) {
+    newTempFilePath(subDir?: string) {
         let genName: () => any = () => {
             let name = rollString(12)
             let filePath
@@ -83,5 +92,17 @@ export class TempDirManager {
                 }
             }
         }
+    }
+
+
+    /**
+     * 写一个文件到临时目录，并且返回其文件名
+     * @param data 数据
+     * @param subPath 子文件夹名
+     */
+    writeFile(data: any, subPath?: string) {
+        let path = this.newTempFilePath(subPath)
+        fs.writeFileSync(path, data)
+        return path
     }
 }
