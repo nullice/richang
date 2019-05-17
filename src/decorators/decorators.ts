80094d1f8338b074f3084872226d2e260d4bad7f
nullice <ui@nullice.com>
2019-05-17 22:29:58 +0800

    decorators

---
+++ src/decorators/decorators.ts
@@ -0,0 +1,29 @@
+export function inset(target: any, propertyKey: string) {
+    if (!target.insetPropertyKeys) {
+        Object.defineProperty(target, "insetPropertyKeys", {
+            enumerable: false,
+            writable: true,
+            value: {}
+        })
+    }
+    ;(<any>target.insetPropertyKeys)[propertyKey] = true
+}
+
+export function configurable(value: boolean) {
+    return function(target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+        descriptor.configurable = value
+    }
+}
+
+export function writable(value: boolean) {
+    return function(target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+        descriptor.writable = value
+    }
+}
+
+export function defineGlobal(constructor: Function) {
+    if (typeof global !== "undefined") {
+        <any>global
+    }
+    console.log(constructor)
+}
