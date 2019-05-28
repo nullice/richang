2a53eee7ad0fd28da92bed51d87dda35ebfbaa1e
nullice <ui@nullice.com>
2019-05-28 23:52:43 +0800

    debug.js

--- debug/debug.js
+++ debug/debug.js
@@ -1,9 +1,9 @@
-// Created by nullice on 2019/01/21 - 01:12 
+// Created by nullice on 2019/01/21 - 01:12
 
 
 window.a={a:123,b:{c:123}}
 window.g = richang.gob.GobFactory(a)
 window.gc = richang.gob.GobFactory.getGobCore(g)
-window.gc.subscribe((o)=>console.log("[subscribe]",o))
-console.log("g",g)
-console.log("gc",gc )
+window.gc.subscribe(o => console.log("[subscribe]", o))
+console.log("g", g)
+console.log("gc", gc)
