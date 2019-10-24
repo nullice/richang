d81fc1a6348fa845ed66c975e457fae47e780456
nullice <ui@nullice.com>
2019-10-24 13:19:18 +0800

    添加 localStorge 测试

--- test/beforeAll.js
+++ test/beforeAll.js
@@ -1,5 +1,7 @@
 // Created by nullice on 2019/05/28 - 22:47
 let fakeIndexeddb = require("fake-indexeddb")
+require('jest-localstorage-mock');
+
 global.window = global
 global.indexedDB = fakeIndexeddb
 global.navigator = {
