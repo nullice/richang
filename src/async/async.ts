e3dbe59ba32981bf526cc025eb4796033c1b97c4
nullice <ui@nullice.com>
2019-11-26 19:45:20 +0800

    + async.when()

--- src/async/async.ts
+++ src/async/async.ts
@@ -10,5 +10,23 @@ export async function sleep(ms: number) {
     })
 }
 
+/**
+ *  等待条件判断
+ *  提供一个函数，会每隔一段时间执行它，当判断为真时，返回
+ *
+ *  @example
+ *  await when(()=>window.isOk == true)
+ *
+ * @param ms 毫秒
+ */
+export async function when(tryFunc: () => boolean, ms = 50) {
+    return new Promise(async function(resolve, reject) {
+        while (!tryFunc()) {
+            await sleep(50)
+        }
+        resolve()
+    })
+}
+
 import { Runner as _Runner } from "./lib/Runner"
 export const Runner = _Runner
